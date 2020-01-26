8a7bf478fa6790b6a41cf3b4151cd3881784484f
Marc Biegota <marc.biegota@5minds.de>
2020-01-26 16:14:16 +0100

    add add-function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 
 fn main() {
     greetings();
+
+    println!("Summe von 10 und 21 ist {}.", add(10, 21));
 }
 
 fn greetings() {
@@ -9,6 +11,11 @@ fn greetings() {
 
     match first_name {
         Some(n) => println!("Hello {}!", n),
-        None => panic!("Hast Du den Vornamen vergessen?"),
+        //None => panic!("Hast Du den Vornamen vergessen?"),
+        None => println!("Hast Du den Vornamen vergessen?"),
     }
+}
+
+fn add(summand_a: i32, summand_b: i32) -> i32 {
+    summand_a + summand_b
 }
