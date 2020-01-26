4ae997f357c6699a92b8ffe5c31df3b6cd4825bc
Marc Biegota <marc.biegota@5minds.de>
2020-01-26 16:23:18 +0100

    add simple closure

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     greetings();
 
     println!("Summe von 10 und 21 ist {}.", add(10, 21));
+    println!(
+        "Summe von 10 und 21 ist auch hier {}.",
+        add_with_closure(10, 21)
+    );
 }
 
 fn greetings() {
@@ -18,4 +22,10 @@ fn greetings() {
 
 fn add(summand_a: i32, summand_b: i32) -> i32 {
     summand_a + summand_b
+}
+
+fn add_with_closure(summand_a: i32, summand_b: i32) -> i32 {
+    let addy_closure = |a: i32, b: i32| -> i32 { a + b };
+
+    addy_closure(summand_a, summand_b)
 }
